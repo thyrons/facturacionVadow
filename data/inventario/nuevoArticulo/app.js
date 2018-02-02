19fc3b774e4e302d60e985a5c51fc95dafe710b6
Willy Narvaez <willynarvaez6@gmail.com>
2018-02-02 18:36:07 -0500

    cambios productos

--- data/inventario/nuevoArticulo/app.js
+++ data/inventario/nuevoArticulo/app.js
@@ -1,4 +1,4 @@
-app.controller("nuevoArticuloController", function ($scope, $state, $http, AuthenticationService,cargarMenuService,cargarEmpresaSucursalService){
+app.controller("nuevoArticuloController", function ($scope, $state, $http, AuthenticationService,cargarMenuService,cargarEmpresaSucursalService, undefinedService){
 	var token;
 	if (localStorage['token']){
     	token = JSON.parse(localStorage['token']);
@@ -36,6 +36,82 @@ app.controller("nuevoArticuloController", function ($scope, $state, $http, Authe
         return array[nb];
     }
     $scope.buttonText = "Guardar Datos"; 
+    $scope.submitForm = function(){         
+        if ($scope.formNuevoArticulo.$valid) {
+            var data = $.param({
+                sucursal: undefinedService.undefinedFunction($scope.recipients),
+                tipo_producto: undefinedService.undefinedFunction($scope.recipientsTipoProducto),
+                iva: undefinedService.undefinedFunction($scope.recipientsIva),
+                codigo : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_1),
+                barras : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_2),
+                nombre : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_3),
+                descripcion : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_4),
+                descuento : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_5),
+                ganancia : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_6),
+                marca : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_7),
+                modelo : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_8),
+                atributo_1 : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_9),
+                descipcion_1 : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_10),
+                atributo_2 : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_11),
+                descipcion_2 : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_12),
+                atributo_3 : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_13),
+                descipcion_3 : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_14),
+                id : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_id),                
+                verificador : undefinedService.undefinedFunction($scope.nuevoArticulo.txt_verificador),
+                tipo: $scope.buttonText
+            });             
+            $http({
+                url: 'data/inventario/nuevoArticulo/app.php',
+                method: "POST",
+                data: data,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }                   
+            })
+            .then(function(response) {                                  
+                if(response.data == 1){
+                    $scope.Sucursal = {};
+                    $scope.formNuevoArticulo.$setPristine();
+                    $scope.buttonText = "Guardar Datos";
+                    bootbox.dialog({
+                        message: "Datos Agregados Correctamente", 
+                        buttons: {
+                            "success" : {
+                                "label" : "Aceptar",
+                                "className" : "btn-sm btn-primary"
+                            }
+                        }
+                    });
+                }
+                else{
+                    if(response.data == 2){
+                        bootbox.dialog({
+                            message: "Error! Al intentar guardar los Datos. Este código ya existe para esta sucursal", 
+                            buttons: {
+                                "danger" : {
+                                    "label" : "Aceptar",
+                                    "className" : "btn-sm btn-danger"
+                                }
+                            }
+                        });  
+                    }else{
+                        bootbox.dialog({
+                            message: "Error! Al intentar guardar los Datos. Comuniquese con el Administrador", 
+                            buttons: {
+                                "danger" : {
+                                    "label" : "Aceptar",
+                                    "className" : "btn-sm btn-danger"
+                                }
+                            }
+                        });  
+                    }             
+                }
+            }, 
+            function(response) {});
+        }else{
+            
+        }
+    };
     jQuery(function($) {        
         $("#loading2").css("display","none");       
         $( "#tabNuevoArticulo" ).click(function( event ) {
--- data/inventario/nuevoArticulo/app.php
+++ data/inventario/nuevoArticulo/app.php
@@ -19,7 +19,7 @@
 		}
 		print_r(json_encode($lista));
 	}
-	if ($_POST['tipo'] == "cargarIva") {		
+	if ($_POST['tipo'] == "cargarIva") {
 		$sql = "select TR.id,TR.descripcion, TR.nombre from tipo_impuesto TI inner join tarifa_impuesto TR on TI.id = TR.id_impuesto where TR.estado = '1'";
 		$sql = $class->consulta($sql);		
 		while ($row = $class->fetch_array($sql)) {
@@ -27,6 +27,18 @@
 		}
 		print_r(json_encode($lista));
 	}
-
-
+	if ($_POST['tipo'] == "Guardar Datos") {
+		$sql = "select id from prodcutos where codigo = '".$_POST['codigo']."' and id_sucursal = '".$_POST['sucursal']."'";
+		$sql = $class->consulta($sql);			
+		if($class->num_rows($sql) > 0) {		
+			echo 2; // PRODUCTO REPETIDO
+		}else{
+			$sql = "INSERT INTO prodcutos (id_sucursal, codigo, codigo_barras, nombre_producto, descripcion, id_tipo_producto, descuento, ganancia, id_iva, marca, modelo, atributo_1, descripcion_1, atributo_2, descripcion_2, atributo_3, descripcion_3, estado, fecha_creacion) values ('".$_POST['sucursal']."', '".$_POST['codigo']."', '".$_POST['barras']."', '".$_POST['nombre']."' '".$_POST['descripcion']."', '".$_POST['tipo_producto']."', '".$_POST['descuento']."', '".$_POST['ganancia']."','".$_POST['iva']."','".$_POST['marca']."','".$_POST['modelo']."','".$_POST['atributo_1']."','".$_POST['descripcion_1']."','".$_POST['atributo_2']."','".$_POST['descripcion_2']."','".$_POST['atributo_3']."','".$_POST['descripcion_3']."','1','".$fecha."')";
+			if($class->consulta($sql)) {
+				echo 1; ///DATOS GUARDADOS
+			}else{
+				echo 4; ///ERROR EN LA BASE
+			}
+		}
+	}
 ?>
